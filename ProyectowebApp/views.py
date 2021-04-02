b3221186c952dd801dec432d9a97e85d529f8462
William <williamvaldez16@gmail.com>
2021-04-01 23:30:26 -0600

    proyecto django

---
+++ ProyectowebApp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, HttpResponse
+
+#from servicios.models import Servicio
+
+# Create your views here.
+
+def home(request):
+
+    return render(request, "ProyectowebApp/home.html")
+
+#def servicios(request):
+
+#    servicios=Servicio.objects.all()
+#    return render(request, "ProyectowebApp/servicios.html",{"servicios": servicios})
+
+#def tienda(request):
+
+#    return render(request, "ProyectowebApp/tienda.html")
+
+#def blog(request):
+
+#    return render(request, "ProyectowebApp/blog.html")
+
+#def contacto(request):
+
+#    return render(request, "ProyectowebApp/contacto.html")
